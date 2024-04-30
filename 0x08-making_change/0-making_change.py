facdbd0f5d7ec704dfbc4d1fd8a09ccaf2d5392e
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2024-04-30 14:37:45 -0700

    task

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,19 +3,21 @@
    to meet a given amount total"""
 
 
-   def makeChange(coins, total):
-    """this function take a list"""
-        if total <= 0;
-            return 0
+def makeChange(coins, total):
+    """This function will take a list of coins and use
+       that to calculate how much change the total will require
+    """
+    if total <= 0:
+        return 0
 
-        else:
-            coin = sorted(coins)
-            coin.reverse()
-            counter = 0
-            for i in coin:
-                while(total >= i):
-                    counter += 1
-                    total -= i
-            if total == 0:
-                return counter
-            return -1
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= i
+        if total == 0:
+            return counter
+        return -1
